a2c7108a5f43d573f8f22a2046563fdd1a285576
Aaron Jackson <ajackson@pobox.com>
2021-08-05 15:11:15 -0500

    Adding mock timer instance to handle test cases.

--- src/test/java/org/battlebots/arena/DefaultSimulatorFactoryTest.java
+++ src/test/java/org/battlebots/arena/DefaultSimulatorFactoryTest.java
@@ -1,6 +1,10 @@
 package org.battlebots.arena;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,11 +19,19 @@ class DefaultSimulatorFactoryTest {
 
     @Test
     public void canCreateSimulator() {
+        Timer timer = Mockito.mock(Timer.class);
+        TimerSingleton.setInstance(timer);
+
         DefaultArenaFactory arenaFactory = new DefaultArenaFactory();
         DefaultSimulatorFactory simulatorFactory = new DefaultSimulatorFactory(arenaFactory);
         Simulator simulator = simulatorFactory.createSimulator();
         assertThat(simulator).isNotNull();
         assertThat(simulator.isRunning()).isFalse();
         assertThat(simulator.getArena()).isNotNull();
+
+        Mockito.verify(timer, Mockito.times(1)).scheduleAtFixedRate(
+                Mockito.any(TimerTask.class),
+                Mockito.eq(100L),
+                Mockito.eq(100L));
     }
 }
